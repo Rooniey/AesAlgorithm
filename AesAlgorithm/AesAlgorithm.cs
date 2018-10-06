0ccfe50e53c2cba1e190f1eebdf08b33e183734d
Rooniey - Marek Maras <210267@edu.p.lodz.pl>
2018-10-06 18:25:35 +0200

    Committted

--- AesAlgorithm/AesAlgorithm.cs
+++ AesAlgorithm/AesAlgorithm.cs
@@ -1,12 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace AesAlgorithm
 {
-    public class Class1
+    public class AesAlgorithm
     {
+        public AesAlgorithm(byte[] key)
+        {
+            keys.Add(key);
+            switch (keys.Count)
+            {
+                case 16:
+                    numberOfRounds = 10;
+                    break;
+
+                case 24:
+                    numberOfRounds = 12;
+                    break;
+
+                case 32:
+                    numberOfRounds = 14;
+                    break;
+            }
+            GenerateKeys();
+        }
+
+        public List<byte[]> Encrypt(List<byte[]> data)
+        {
+            return null;
+        }
+
+        public List<byte[]> Decrypt(List<byte[]> encryptedData)
+        {
+            return null;
+        }
+
+        private void GenerateKeys()
+        {
+
+        }
+
+        private void MixColumns()
+        {
+
+        }
+
+        private void AddRoundKey()
+        {
+
+        }
+
+        private int numberOfRounds;
+        private List<byte[]> keys = new List<byte[]>();
     }
 }
