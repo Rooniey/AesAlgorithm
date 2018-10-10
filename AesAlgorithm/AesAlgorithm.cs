b9da35edb4be1229d0ae93de44dd768ac9bf0680
mamajjo <maciek21197@gmail.com>
2018-10-10 19:32:37 +0200

    changed shifting function

--- AesAlgorithm/AesAlgorithm.cs
+++ AesAlgorithm/AesAlgorithm.cs
@@ -62,15 +62,34 @@ namespace AesAlgorithm
             }
         }
 
-        public void ShiftRows(byte[,] a)
+        public byte[,] ShiftRows(byte[,] a)
         {
-            for (int i = 1; i < STATE_ROWS; i++)
+            byte[,] b = new byte[STATE_ROWS, STATE_COLUMNS];
+
+            b = a;
+            byte temp1 = a[1, 0];
+            for (int i = 1; i < STATE_COLUMNS; i++)
             {
-                for (int j = 0; j < STATE_COLUMNS; j++)
-                {
-                    a[i, j] = (byte)(a[i, j] << i);
-                }
+                b[1, i - 1] = b[1, i];
             }
+            b[1, 3] = temp1;
+            temp1 = a[2, 0];
+            byte temp2 = a[2, 1];
+            for (int i = 2; i < STATE_COLUMNS; i++)
+            {
+                b[2, i - 2] = b[2, i];
+            }
+            b[2, 2] = temp1;
+            b[2, 3] = temp2;
+
+            temp1 = a[3, 0];
+            temp2 = a[3, 1];
+            byte temp3 = a[3, 2];
+            b[3, 0] = b[3, 3];
+            b[3, 1] = temp1;
+            b[3, 2] = temp2;
+            b[3, 3] = temp3;
+            return b;
         }
 
         public void MixColumns(byte[,] state)
