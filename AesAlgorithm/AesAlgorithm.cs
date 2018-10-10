bd5202101c2faca370101ef55891b6cb9922acb7
mamajjo <maciek21197@gmail.com>
2018-10-10 23:15:00 +0200

    changed into the algorithm

--- AesAlgorithm/AesAlgorithm.cs
+++ AesAlgorithm/AesAlgorithm.cs
@@ -57,7 +57,7 @@ namespace AesAlgorithm
             {
                 for (int j = 0; j < STATE_COLUMNS; j++)
                 {
-                    a[i, j] = TableConstants.RijndaelSBox[GetRightPartIndex(a[i, j]), GetLeftPartIndex(a[i, j])];
+                    a[i, j] = TableConstants.RijndaelSBox[GetLeftPartIndex(a[i, j]), GetRightPartIndex(a[i, j])];
                 }
             }
         }
@@ -66,31 +66,31 @@ namespace AesAlgorithm
         {
             byte[,] b = new byte[STATE_ROWS, STATE_COLUMNS];
 
-            b = a;
-            byte temp1 = a[1, 0];
-            for (int i = 1; i < STATE_COLUMNS; i++)
+            for (int i = 0; i < STATE_ROWS; i++)
             {
-                b[1, i - 1] = b[1, i];
+                for (int j = 0; j < STATE_COLUMNS; j++)
+                {
+                    b[i, j] = a[i, (j + i) % STATE_COLUMNS];
+                }
             }
-            b[1, 3] = temp1;
-            temp1 = a[2, 0];
-            byte temp2 = a[2, 1];
-            for (int i = 2; i < STATE_COLUMNS; i++)
-            {
-                b[2, i - 2] = b[2, i];
-            }
-            b[2, 2] = temp1;
-            b[2, 3] = temp2;
 
-            temp1 = a[3, 0];
-            temp2 = a[3, 1];
-            byte temp3 = a[3, 2];
-            b[3, 0] = b[3, 3];
-            b[3, 1] = temp1;
-            b[3, 2] = temp2;
-            b[3, 3] = temp3;
             return b;
         }
+
+        public byte[,] ReverseShiftRows(byte[,] a)
+        {
+            byte[,] b = new byte[STATE_ROWS, STATE_COLUMNS];
+            for (int i = 0; i < STATE_ROWS; i++)
+            {
+                for (int j = STATE_COLUMNS-1; j > 0; j--)
+                {
+                    b[i, j] = a[i, (j + i) % STATE_COLUMNS];
+                }
+            }
+            return b;
+            
+        }
+
 
         public void MixColumns(byte[,] state)
         {
