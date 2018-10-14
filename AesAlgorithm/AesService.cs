87de1294cbb8f3c21a3ef00c3917ec8cdbddf1e0
Marek Maras <marek.maras2@gmail.com>
2018-10-14 18:35:44 +0200

    Added hex display and merged

--- AesAlgorithm/AesService.cs
+++ AesAlgorithm/AesService.cs
@@ -8,20 +8,17 @@ namespace Cryptography
     public class AesService : ISymmetricCryptoService
     {
         private readonly IConvertProcessor<List<byte[,]>, byte[]> _dataProcessor;
-        private readonly IConvertProcessor<List<byte[,]>, byte[]> _padDataProcessor;
         private readonly IAlgorithmFactory _algorithmFactory;
         
         public AesService()
         {
-            _dataProcessor = new BlockProcessor();
-            _padDataProcessor = new BlockPaddingProcessor();
-            _algorithmFactory = new AesAlgorithmFactory(new BlockPaddingProcessor());
+            _dataProcessor = new BlockDataProcessor();
+            _algorithmFactory = new AesAlgorithmFactory(new BlockDataProcessor());
         }
 
         public byte[] Encrypt(byte[] key, IDataSource dataSource)
         {
-            //TODO implement new data processor; adding padding and 16 bytes (how many bytes were padded)
-            List<byte[,]> blocks = _padDataProcessor.ConvertTo(dataSource.GetData());
+            List<byte[,]> blocks = _dataProcessor.ConvertTo(dataSource.GetData());
             AesAlgorithmImp aes = _algorithmFactory.GetAlgorithm(key);
             List<byte[,]> encryptedBlocks = aes.Encrypt(blocks);
             return _dataProcessor.ConvertBack(encryptedBlocks);
@@ -32,8 +29,7 @@ namespace Cryptography
             List<byte[,]> blocks = _dataProcessor.ConvertTo(dataSource.GetData());
             AesAlgorithmImp aes = _algorithmFactory.GetAlgorithm(key);
             List<byte[,]> decryptedBlocks = aes.Decrypt(blocks);
-            //TODO converter get last 2 blocks; read how many bytes were padded; remove padded bytes
-            return _padDataProcessor.ConvertBack(decryptedBlocks);
+            return _dataProcessor.ConvertBack(decryptedBlocks);
         }
 
         
--- WpfGui/Validators/AesCipherkeyValidator.cs
+++ WpfGui/Validators/AesCipherkeyValidator.cs
@@ -1,4 +1,6 @@
-﻿using FluentValidation;
+﻿using System.Linq;
+using Cryptography.Constants;
+using FluentValidation;
 using WpfGui.Models;
 
 namespace WpfGui.Validators
@@ -10,6 +12,9 @@ namespace WpfGui.Validators
             RuleFor(model => model.CurrentLength)
                 .Must((model, length) => length == model.CipherkeyLength)
                 .WithMessage("Provide key with correct number of bits");
+            RuleFor(model => model.CipherkeyLength)
+                .Must(chosenLength => AesParameters.KEY_LENGTHS.Contains(chosenLength))
+                .WithMessage("Chosen length should be of size 128, 192, 256 bits length");
         }
     }
 }
